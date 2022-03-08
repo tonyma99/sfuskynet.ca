4d2042896fdbf76ebceb4178429cdf35292eeb2c
Tony Ma <tonyphma@gmail.com>
2022-03-08 01:09:52 -0800

    Toggle class instead of add/remove

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,10 +1,5 @@
 function toggleMenu() {
     const navElement = document.getElementsByTagName("nav")[0];
-    if (navElement.classList.contains("hidden")) {
-        navElement.classList.remove("hidden");
-        navElement.classList.add("flex");
-    } else {
-        navElement.classList.add("hidden");
-        navElement.classList.remove("flex");
-    }
+    navElement.classList.toggle("hidden");
+    navElement.classList.toggle("flex");
 }
