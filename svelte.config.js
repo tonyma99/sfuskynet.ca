14c7601a1c38a4f127357613904bd8ae0977ae32
Tony Ma <tonyphma@gmail.com>
2023-01-30 01:54:11 -0800

    Enable static site generation

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter()
-	}
+import adapter from '@sveltejs/adapter-static';
+ 
+export default {
+  kit: {
+    adapter: adapter(),
+  }
 };
-
-export default config;
