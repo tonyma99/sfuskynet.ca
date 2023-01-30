24e45123f7294a721772910f48faa65381fc22c3
Tony Ma <tonyphma@gmail.com>
2023-01-30 01:39:42 -0800

    Modify layout on team page

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -2,12 +2,16 @@
     import Team from "./Team.svelte";
     import Profile from "./Profile.svelte";
 
+    $: innerWidth = 0;
+    $: mobile = innerWidth <= 768;
+
     const teams = [
         {
             name: 'Mechanics',
             headline: 'Practical design of robotic mechanisms',
             image: '/mechanics.jpg',
             description: 'The Mechanics team gives shape and character to our team of robots and is a great way to get started in the club. Members are introduced to the basics of CAD and work on designing components such as base-plates, motor mounts and kicker systems, usually under the supervision of more senior members. During this process, the mechanics team focuses on creating an efficient design workflow and generating documentation so that future team members can learn about how our robots have evolved over time. Mechanics members primarily use SolidWorks to familiarize themselves with the basics of mechanical design and experience teamwork in a production environment.',
+            id: 'mechanics-team',
             tags: [
                 '3D Printing',
                 'CNC Milling',
@@ -19,6 +23,7 @@
             headline: 'Design and test robot PCBs and firmware',
             image: '/electronics.jpg',
             description: 'The electronics team designs and tests all the electrical components on the robots, including creating custom PCBs from scratch. Our modular PCB design integrates multiple daughterboards, including the motor driver, kicker driver and dribbler motor driver boards onto a central motherboard using removable connectors. This team gives its members experience using Altium Designer, an industry standard PCB design tool, working through the entire design process from schematic capture to PCB routing. In addition, members of the electronics team work a lot on testing and troubleshooting all parts of the design to work on improvements for future robot generations.',
+            id: 'electronics-team',
             tags: [
                 'Altium Designer',
                 'C',
@@ -31,6 +36,7 @@
             headline: 'Building the brains of our system',
             image: '/software.jpg',
             description: 'At the software team our members collaborate together to develop the core and support elements of our software system. As speed gives us a competitive advantage we use a C++ tech stack to develop our AI and robotic control algorithms as well as our interface. We are looking for motivated students who are interested in applying their knowledge across and beyond the curriculum into practice. We are currently working on developing our robotic command and control systems, our data systems and improving our GUI among other tasks. In our team we hope you not only gain technical experience but also experience in collaborating with your peers through pair programming, code reviews, documentation and meetings.',
+            id: 'software-team',
             tags: [
                 'Boost',
                 'C++',
@@ -83,6 +89,8 @@
     ];
 </script>
 
+<svelte:window bind:innerWidth />
+
 <svelte:head>
     <title>SFU Robot Soccer - Team</title>
 </svelte:head>
@@ -92,17 +100,30 @@
         <h1>Team</h1>
         <p>At the SFU Robot Soccer Club, our team members come from different majors, experience levels and backgrounds. Our core teams are our <b>Mechanics</b>, <b>Electronics</b> and <b>Software</b> team where we work together to get our autonomous robots competition ready.</p>
     </div>
-    
-    <div class="section--teams">
-        {#each teams as team}
-            <Team name={team.name} headline={team.headline} image={team.image} description={team.description} tags={team.tags} />
-        {/each}
+
+    {#if mobile}
+    <div class="section--nav">
+        <h4>Jump to section</h4>
+        <ul>
+            <li><a href="#executive-team">Executive Team</a></li>
+            <li><a href="#mechanics-team">Mechanics Team</a></li>
+            <li><a href="#electronics-team">Electronics Team</a></li>
+            <li><a href="#software-team">Software Team</a></li>
+        </ul>
     </div>
+    {/if}
 
     <div class="section--execs">
-        <h3>Executive Team</h3>
+        <h3 id="executive-team">Executive Team</h3>
         {#each execs as exec}
             <Profile name={exec.name} position={exec.position} profile={exec.profile} linkedin={exec.linkedin} email={exec.email} />
+        {/each}
+    </div>
+    
+    <div class="section--teams">
+        <h3>Teams</h3>
+        {#each teams as team, idx}
+            <Team idx={idx} name={team.name} headline={team.headline} image={team.image} description={team.description} id={team.id} tags={team.tags} />
         {/each}
     </div>
 </div>
@@ -125,5 +146,15 @@
 
     h3 {
         font-size: 2em;
+    }
+
+    h4 {
+        margin-bottom: 1em;
+    }
+
+    ul, li {
+        list-style-type: none;
+        padding-left: 0;
+        color: #777;
     }
 </style>
