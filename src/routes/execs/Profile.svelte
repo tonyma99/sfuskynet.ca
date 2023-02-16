2629ba1a9df55a6b995b43377134c73932c1cf1d
Tony Ma <tonyphma@gmail.com>
2023-02-15 22:30:23 -0800

    Modify profile design

--- src/routes/execs/Profile.svelte
+++ src/routes/execs/Profile.svelte
@@ -6,34 +6,41 @@
 </script>
 
 <div>
-    <img class="profile--photo" src={profile} alt="{name}'s profile picture" width="128px">
-    <p class="profile--name">{name}</p>
-    <p class="profile--position">{position}</p>
+    <img class="profile--photo" src={profile} alt="{name}'s profile picture">
+    <p>{name}</p>
+    <p>{position}</p>
     <a href="{linkedin}"><i class="fa-brands fa-linkedin-in"></i></a>
 </div>
 
 <style>
     div {
         display: inline-block;
-        margin: 1rem;
-        width: 168px;
+        margin: 0.5rem;
+        width: 10rem;
         vertical-align: top;
     }
 
-    .profile--photo {
+    img {
         border-radius: 50%;
+        width: 80%;
     }
 
-    .profile--name {
-        font-size: 1em;
-        font-weight: 600;
-        margin: 6px 0;
+    p {
         line-height: 1;
+        margin: 0;
+        margin-top: 6px;
+    }
+
+    p:nth-of-type(1) {
+        font-size: 1rem;
+        font-weight: 600;
     }
     
-    .profile--position {
-        font-size: 0.9em;
-        margin: 0;
-        line-height: 1;
+    p:nth-of-type(2) {
+        font-size: 0.8rem;
+    }
+
+    i {
+        font-size: 0.9rem;
     }
 </style>
