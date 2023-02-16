33159b9c66149ed2a0b54e85e201b26a58e2d2ec
Tony Ma <tonyphma@gmail.com>
2023-02-16 00:10:04 -0800

    Fix mobile view cards

--- src/lib/components/PageCardContainer.svelte
+++ src/lib/components/PageCardContainer.svelte
@@ -6,4 +6,11 @@
     div {
         display: flex;
     }
+
+    @media only screen and (max-width: 768px) {
+        div {
+            display: inline-block;
+            max-width: 320px;
+        }
+    }
 </style>
