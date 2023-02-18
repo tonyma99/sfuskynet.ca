42f26ad557ac7d0945b8ffddadc51ababde9e310
Tony Ma <tonyphma@gmail.com>
2023-02-17 17:16:23 -0800

    Finalize email settings

--- src/routes/api/message/+server.js
+++ src/routes/api/message/+server.js
@@ -1,24 +1,39 @@
-import { SENDGRID_API_KEY } from '$env/static/private'
+import { EMAIL, SENDGRID_API_KEY } from '$env/static/private'
 import sgMail from '@sendgrid/mail'
 
 sgMail.setApiKey(SENDGRID_API_KEY)
 
 export async function POST({ request }) {
-    const body = await request.json()
-    const text = `Name: ${body.name}\nEmail: (${body.email})\n---\n\n${body.message}`
+    const { name, email, message } = await request.json()
 
     try {
         const msg = {
-            to: 'matonym@sfu.ca',
-            from: 'matonym@sfu.ca', 
-            subject: 'SFU Robot Soccer Club Website Message',
-            text: text,
+            to: EMAIL,
+            from: {
+                email: EMAIL,
+                name: 'SFU Robot Soccer (Website)'
+            },
+            replyTo: {
+                email,
+                name
+            },
+            content: [
+                {
+                    type: 'text/html',
+                    value: `
+                        <p>
+                            Name: <strong>${name}</strong><br>
+                            Email: <strong>${email}</strong>
+                        </p>
+                        <hr/>
+                        <p style="white-space: pre-wrap;">${message}</p>`
+                }
+              ],
+            subject: 'SFU Robot Soccer - Message'
         }
         await sgMail.send(msg)
-    } catch (e) {
-        console.error(e)
-        return new Response({ status: 500 })
+    } catch {
+        return new Response(null, { status: 500 })
     }
-   
-    return new Response({ status: 200 })
+    return new Response(null, { status: 200 })
 }
