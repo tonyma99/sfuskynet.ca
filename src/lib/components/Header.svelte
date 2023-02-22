c5b4236a06cc968d647b3d7cd7ce0b8e0c75d641
Tony Ma <tonyphma@gmail.com>
2023-02-22 12:39:00 -0800

    Change mobile menu icon to Material Icons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,145 +1,137 @@
 <script>
-    import { navigating, page } from '$app/stores';
+  import { navigating, page } from '$app/stores'
 
-    let menu = false;
+  let menu = false
 
-    let innerWidth
-    $: if (innerWidth <= 768 || $navigating) menu = false
+  let innerWidth
+  $: if (innerWidth <= 768 || $navigating) menu = false
 
-    function toggle() {
-        menu = menu ? false : true;
-    }
+  function toggle() {
+    menu = menu ? false : true
+  }
 
-    const links = [
-        { path: '/', name: 'Home' },
-        { path: '/teams', name: 'Teams' },
-        { path: '/execs', name: 'Execs' },
-        { path: '/sponsors', name: 'Sponsors' },
-        { path: '/contact', name: 'Contact'}
-    ]
+  const links = [
+    { path: '/', name: 'Home' },
+    { path: '/teams', name: 'Teams' },
+    { path: '/execs', name: 'Execs' },
+    { path: '/sponsors', name: 'Sponsors' },
+    { path: '/contact', name: 'Contact' }
+  ]
 </script>
 
 <svelte:window bind:innerWidth />
 
 <header>
+  <div>
+    <a href="/">
+      <img src="/images/logo.png" alt="SFU Robot Socccer Club team logo" width="42" height="42" />
+    </a>
+  </div>
+  {#if innerWidth !== undefined}
     <div>
-        <a href="/">
-            <img src="/images/logo.png" alt="SFU Robot Socccer Club team logo" width=42 height=42>
-        </a>
-    </div>
-    {#if innerWidth !== undefined}
-    <div>
-        {#if innerWidth > 768} 
+      {#if innerWidth > 768}
         <ul>
-            {#each links as link}
-            <li><a href={link.path} on:click={toggle} aria-current="{$page.url.pathname === link.path}">{link.name}</a></li>
-            {/each}
+          {#each links as link}
+            <li>
+              <a href={link.path} on:click={toggle} aria-current={$page.url.pathname === link.path}>{link.name}</a>
+            </li>
+          {/each}
         </ul>
-        {:else}
-        <span on:click={toggle} on:keypress={toggle}>
-            {#if !menu}
-            <i class="fa-solid fa-bars fa-lg"></i>
-            {:else}
-            <i class="fa-solid fa-xmark fa-lg"></i>
-            {/if}
-        </span>
-        {/if}
+      {:else}
+        <span class="material-icons-round" on:click={toggle} on:keypress={toggle}>{!menu ? 'menu' : 'close'} </span>
+      {/if}
     </div>
-    {/if}
-    {#if menu && innerWidth <= 768}
+  {/if}
+  {#if menu && innerWidth <= 768}
     <div id="mobile">
-        <ul>
-            {#each links as link}
-            <li><a href={link.path} on:click={toggle} aria-current="{$page.url.pathname === link.path}">{link.name}</a></li>
-            {/each}
-        </ul>
+      <ul>
+        {#each links as link}
+          <li>
+            <a href={link.path} on:click={toggle} aria-current={$page.url.pathname === link.path}>{link.name}</a>
+          </li>
+        {/each}
+      </ul>
     </div>
-    {/if}
+  {/if}
 </header>
 
 <style>
+  header {
+    align-items: center;
+    display: flex;
+    height: 64px;
+    padding: 0 48px;
+    z-index: 1;
+    font-size: 1.05rem;
+  }
+
+  header > :nth-child(1) {
+    flex: 1;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  header > :nth-child(2) {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  a {
+    transition: 0.3s;
+  }
+
+  a:hover {
+    color: #777;
+  }
+
+  li {
+    display: inline-flex;
+  }
+
+  li + li {
+    margin-left: 16px;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  span {
+    padding: 0.5rem;
+    font-size: 36px;
+  }
+
+  #mobile {
+    position: absolute;
+    background-color: #f8f8f8;
+    left: 0;
+    top: 64px;
+    width: 100%;
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
+    background-color: #f0f0f0;
+  }
+
+  #mobile a {
+    display: block;
+    font-size: 1.2em;
+  }
+
+  #mobile li {
+    text-align: center;
+    margin-left: 0;
+    display: block;
+    padding: 6px;
+  }
+
+  [aria-current]:not([aria-current='false']) {
+    color: #a6192e;
+  }
+
+  @media only screen and (max-width: 768px) {
     header {
-        align-items: center;
-        display: flex;
-        height: 64px;
-        padding: 0 48px;
-        z-index: 1;
-        font-size: 1.05rem;
+      padding: 0 24px;
     }
-
-    header > :nth-child(1) {
-        flex: 1;
-        display: flex;
-        justify-content: flex-start;
-    }
-
-    header > :nth-child(2) {
-        flex: 1;
-        display: flex;
-        justify-content: flex-end;
-    }
-
-    a {
-        transition: 0.3s;
-    }
-
-    a:hover {
-        color: #777;
-    }
-
-    li {
-        display: inline-flex;
-    }
-
-    li + li {
-        margin-left: 16px;
-    }
-
-    ul {
-        list-style: none;
-        padding-left: 0;
-    }
-
-    span {
-        background: none;
-        border: none;
-        padding: 0.5rem;
-    }
-
-    span i {
-        vertical-align: middle;
-        padding: 6px;
-    }
-
-    #mobile {
-        position: absolute;
-        background-color: #f8f8f8;
-        left: 0;
-        top: 64px;
-        width: 100%;
-        box-shadow: 0 4px 4px rgba(0, 0, 0, 0.2);
-        background-color: #f0f0f0;
-    }
-
-    #mobile a {
-        display: block;
-        font-size: 1.2em;
-    }
-
-    #mobile li {
-        text-align: center;
-        margin-left: 0;
-        display: block;
-        padding: 6px;
-    }
-
-    [aria-current]:not([aria-current="false"]) {
-        color: #a6192e;
-    }
-
-    @media only screen and (max-width: 768px) {
-        header {
-            padding: 0 24px;
-        }
-    }
+  }
 </style>
