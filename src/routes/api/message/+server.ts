0c8bcc873f92abd58896795a2fc7c1e414b572fa
Tony Ma <tonyphma@gmail.com>
2023-02-22 19:50:05 -0800

    Format code

--- src/routes/api/message/+server.ts
+++ src/routes/api/message/+server.ts
@@ -5,31 +5,31 @@ import sgMail from '@sendgrid/mail'
 sgMail.setApiKey(SENDGRID_API_KEY)
 
 export const POST: RequestHandler = async ({ request }) => {
-  const { name, email, message } = await request.json()
+	const { name, email, message } = await request.json()
 
-  try {
-    const msg = {
-      to: EMAIL,
-      from: {
-        email: EMAIL,
-        name: 'SFU Robot Soccer (Website)'
-      },
-      replyTo: {
-        email,
-        name
-      },
-      html: `
+	try {
+		const msg = {
+			to: EMAIL,
+			from: {
+				email: EMAIL,
+				name: 'SFU Robot Soccer (Website)'
+			},
+			replyTo: {
+				email,
+				name
+			},
+			html: `
         <p>
           Name: <strong>${name}</strong><br>
           Email: <strong>${email}</strong>
         </p>
         <hr/>
         <p style="white-space: pre-wrap;">${message}</p>`,
-      subject: 'SFU Robot Soccer - Message'
-    }
-    await sgMail.send(msg)
-  } catch {
-    return new Response(null, { status: 500 })
-  }
-  return new Response(null, { status: 200 })
+			subject: 'SFU Robot Soccer - Message'
+		}
+		await sgMail.send(msg)
+	} catch {
+		return new Response(null, { status: 500 })
+	}
+	return new Response(null, { status: 200 })
 }
