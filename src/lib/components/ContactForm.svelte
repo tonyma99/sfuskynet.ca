9ce41c350cee10e4e81ed9d8600c061253c94937
Tony Ma <tonyphma@gmail.com>
2023-02-22 16:21:39 -0800

    Add `eslint` and `prettier`

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -1,93 +1,93 @@
 <script lang="ts">
-  let success = false
-  let alert = false
-  let disabled = false
-  let color: string
-  $: alert ? (color = '#376e37') : (color = '#a6192e')
+	let success = false
+	let alert = false
+	let disabled = false
+	let color: string
+	$: alert ? (color = '#376e37') : (color = '#a6192e')
 
-  const handleSubmit = async (event: Event) => {
-    disabled = true
-    const formElement = event.target as HTMLFormElement
-    const formData = new FormData(formElement)
-    formElement.reset()
-    const result = await fetch('/api/message', {
-      method: 'POST',
-      body: JSON.stringify(Object.fromEntries(formData)),
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-    if (result.status === 200) success = true
-    alert = true
-    disabled = false
-  }
+	const handleSubmit = async (event: Event) => {
+		disabled = true
+		const formElement = event.target as HTMLFormElement
+		const formData = new FormData(formElement)
+		formElement.reset()
+		const result = await fetch('/api/message', {
+			method: 'POST',
+			body: JSON.stringify(Object.fromEntries(formData)),
+			headers: {
+				'content-type': 'application/json'
+			}
+		})
+		if (result.status === 200) success = true
+		alert = true
+		disabled = false
+	}
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
-  <input name="name" type="text" placeholder="Full name" required />
-  <input name="email" type="email" placeholder="Email" required />
-  <textarea name="message" placeholder="Message" required />
-  <button {disabled}>Submit</button>
+	<input name="name" type="text" placeholder="Full name" required />
+	<input name="email" type="email" placeholder="Email" required />
+	<textarea name="message" placeholder="Message" required />
+	<button {disabled}>Submit</button>
 </form>
 {#if alert}
-  <div>
-    <p style:color>
-      {#if success}
-        Message successfully sent.
-      {:else}
-        Message failed to send.
-      {/if}
-    </p>
-  </div>
+	<div>
+		<p style:color>
+			{#if success}
+				Message successfully sent.
+			{:else}
+				Message failed to send.
+			{/if}
+		</p>
+	</div>
 {/if}
 
 <style>
-  form {
-    display: flex;
-    gap: 6px;
-    flex-direction: column;
-    max-width: 576px;
-    margin: auto;
-  }
+	form {
+		display: flex;
+		gap: 6px;
+		flex-direction: column;
+		max-width: 576px;
+		margin: auto;
+	}
 
-  form > * {
-    background-color: #fafafa;
-    width: 100%;
-    padding: 6px 12px;
-    border-radius: 0.5em;
-    border: 1px solid #d0d0d0;
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-size: 1rem;
-  }
+	form > * {
+		background-color: #fafafa;
+		width: 100%;
+		padding: 6px 12px;
+		border-radius: 0.5em;
+		border: 1px solid #d0d0d0;
+		font-family: 'IBM Plex Sans', sans-serif;
+		font-size: 1rem;
+	}
 
-  form button {
-    transition: 0.3s;
-    background-color: #a6192e;
-    border: none;
-    color: #f0f0f0;
-    font-weight: bold;
-    padding: 8px;
-  }
+	form button {
+		transition: 0.3s;
+		background-color: #a6192e;
+		border: none;
+		color: #f0f0f0;
+		font-weight: bold;
+		padding: 8px;
+	}
 
-  form button:hover {
-    background-color: #cc0633;
-  }
+	form button:hover {
+		background-color: #cc0633;
+	}
 
-  form button:disabled {
-    background-color: #999;
-  }
+	form button:disabled {
+		background-color: #999;
+	}
 
-  form button:focus,
-  form button:active {
-    background-color: #a6192e;
-  }
+	form button:focus,
+	form button:active {
+		background-color: #a6192e;
+	}
 
-  form textarea {
-    height: 192px;
-    resize: none;
-  }
+	form textarea {
+		height: 192px;
+		resize: none;
+	}
 
-  p {
-    color: var(alertColor);
-  }
+	p {
+		color: var(alertColor);
+	}
 </style>
--- src/routes/execs/+page.svelte
+++ src/routes/execs/+page.svelte
@@ -1,61 +1,78 @@
 <script lang="ts">
-  import { ContentPageLayout, Profile } from '$lib/components'
-  export let data: {
-    executives: Array<Person>
-    directors: Array<Person>
-    leads: Array<Person>
-    heads: Array<Person>
-  }
+	import { ContentPageLayout, Profile } from '$lib/components'
+	import type { Person } from '$lib/types'
+
+	export let data: {
+		executives: Array<Person>
+		directors: Array<Person>
+		leads: Array<Person>
+		heads: Array<Person>
+	}
 </script>
 
 <ContentPageLayout titleText="Execs" headingText="Executive Team">
-  <div>
-    <h2>Executives</h2>
-    <div>
-      {#each data.executives as executive}
-        <Profile
-          name={executive.name}
-          position={executive.position}
-          image={executive.image}
-          linkedin={executive.linkedin}
-        />
-      {/each}
-    </div>
-  </div>
+	<div>
+		<h2>Executives</h2>
+		<div>
+			{#each data.executives as executive}
+				<Profile
+					name={executive.name}
+					position={executive.position}
+					image={executive.image}
+					linkedin={executive.linkedin}
+				/>
+			{/each}
+		</div>
+	</div>
 
-  <div>
-    <h2>Directors</h2>
-    <div>
-      {#each data.directors as exec}
-        <Profile name={exec.name} position={exec.position} image={exec.image} linkedin={exec.linkedin} />
-      {/each}
-    </div>
-  </div>
+	<div>
+		<h2>Directors</h2>
+		<div>
+			{#each data.directors as exec}
+				<Profile
+					name={exec.name}
+					position={exec.position}
+					image={exec.image}
+					linkedin={exec.linkedin}
+				/>
+			{/each}
+		</div>
+	</div>
 
-  <div>
-    <h2>Leads</h2>
-    <div>
-      {#each data.leads as exec}
-        <Profile name={exec.name} position={exec.position} image={exec.image} linkedin={exec.linkedin} />
-      {/each}
-    </div>
-  </div>
+	<div>
+		<h2>Leads</h2>
+		<div>
+			{#each data.leads as exec}
+				<Profile
+					name={exec.name}
+					position={exec.position}
+					image={exec.image}
+					linkedin={exec.linkedin}
+				/>
+			{/each}
+		</div>
+	</div>
 
-  <div>
-    <h2>Heads</h2>
-    <div>
-      {#each data.heads as exec}
-        <Profile name={exec.name} position={exec.position} image={exec.image} linkedin={exec.linkedin} />
-      {/each}
-    </div>
-  </div>
+	<div>
+		<h2>Heads</h2>
+		<div>
+			{#each data.heads as exec}
+				<Profile
+					name={exec.name}
+					position={exec.position}
+					image={exec.image}
+					linkedin={exec.linkedin}
+				/>
+			{/each}
+		</div>
+	</div>
 </ContentPageLayout>
 
 <style>
-  div > div {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 1rem;
-  }
+	div > div {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		gap: 1rem;
+	}
 </style>
